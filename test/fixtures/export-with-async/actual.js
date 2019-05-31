7ca8da46b34b92487dc4b87973398982fd1454ae
Theo Sun <theo.sun@sap.com>
2019-05-31 11:07:17 +0800

    remove(async): feature

---
+++ test/fixtures/export-with-async/actual.js
@@ -0,0 +1,8 @@
+const fetchCurrentUserInformation = async () => {
+  var res = await fetch("/api/v1/user/");
+  var body = await res.json();
+  return body;
+};
+
+
+export { fetchCurrentUserInformation };

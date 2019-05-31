ac2004d0e323df8d1d0abee647d5234de1fd229a
Theo Sun <theo.sun@sap.com>
2019-05-31 11:13:37 +0800

    feat(async): func

--- test/fixtures/export-with-async/expected.js
+++ test/fixtures/export-with-async/expected.js
@@ -3,33 +3,27 @@ sap.ui.define("babel/test/test/fixtures/export-with-async/actual", [], function 
 
   var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-  var _this = this;
-
-  var fetchCurrentUserInformation = function fetchCurrentUserInformation() {
-    var res, body;
-    return regeneratorRuntime.async(function fetchCurrentUserInformation$(_context) {
-      while (1) {
-        switch (_context.prev = _context.next) {
-          case 0:
-            _context.next = 2;
-            return regeneratorRuntime.awrap(fetch("/api/v1/user/"));
-
-          case 2:
-            res = _context.sent;
-            _context.next = 5;
-            return regeneratorRuntime.awrap(res.json());
-
-          case 5:
-            body = _context.sent;
-            return _context.abrupt("return", body);
-
-          case 7:
-          case "end":
-            return _context.stop();
-        }
+  function _async(fn) {
+    return function () {
+      var args = Array.prototype.slice.call(arguments, 0);try {
+        return Promise.resolve(fn.apply(this, args));
+      } catch (e) {
+        return Promise.reject(e);
       }
-    }, null, _this);
-  };
+    };
+  }function _await(input, then, direct) {
+    if (direct) {
+      return typeof then === 'function' ? then(input) : input;
+    }if (typeof then === 'function') {
+      return Promise.resolve(input).then(then);
+    } else {
+      return Promise.resolve(input);
+    }
+  }var fetchCurrentUserInformation = _async(function () {
+    return _await(fetch("/api/v1/user/"), function (res) {
+      return _await(res.json());
+    });
+  });
 
   _default = _extends({
     fetchCurrentUserInformation: fetchCurrentUserInformation

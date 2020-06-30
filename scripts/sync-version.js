b26f6a8f553cdf81a79bf482b139cf576c7da7f4
Theo Sun <theo.sun@outlook.com>
2020-06-30 20:18:34 +0800

    chore: dep update

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -1,11 +1,21 @@
 var fs = require("fs")
 var path = require("path")
+var { forEach, has } = require("@newdash/newdash")
 
 var pluginPackageJson = require("../package")
 var presetPackageJson = require("../preset/package")
 
 presetPackageJson.version = pluginPackageJson.version
 presetPackageJson.dependencies["babel-plugin-ui5-next"] = pluginPackageJson.version
+
+// copy plugins versions
+forEach(presetPackageJson.dependencies, (value, name) => {
+  if (has(pluginPackageJson.devDependencies, name)) {
+    presetPackageJson.dependencies[name] = pluginPackageJson.devDependencies[name]
+  }
+})
+
+console.log(presetPackageJson)
 
 fs.writeFileSync(
   path.join(__dirname, "../preset/package.json"),
